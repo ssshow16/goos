e2915bc3aadae0cb994b2ae7847ca39cd5a93c2e
신정안 <bruce.shin@onestorecorp.com>
2022-03-07 09:15:45 +0900

    12.2.1  입찰 테스트 케이스 추가

--- test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -13,6 +13,12 @@ public class AuctionSniperEndToEndTest {
         auction.startSellingItem();
         application.startBiddingIn(auction); // 메시지 전송 XMPP 및 UI Open
         auction.hasReceivedJoinRequestFromSniper(); // XMPP로 부터 메시지 수신여부를 확인한다
+
+        auction.reportPrice(1000,98,"other bidder"); // 가격:1000, 증액 98, 'other bidder' 이름으로 메시지 전송
+        application.hasShownSniperIsBidding(); // acution으로 부터 생긴 메시지 노출 확인
+
+        auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);//
+
         auction.announceClosed(); // 종료메시지 전송
         application.showsSniperHasLostAuction(); //화면에 Lost 출력여부를 확인한다
     }
