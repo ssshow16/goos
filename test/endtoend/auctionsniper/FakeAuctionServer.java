3fa11027736624d16c35a048caafcee852c37e34
신정안 <bruce.shin@onestorecorp.com>
2022-03-07 10:58:18 +0900

    종료메시지 전송

--- test/endtoend/auctionsniper/FakeAuctionServer.java
+++ test/endtoend/auctionsniper/FakeAuctionServer.java
@@ -70,7 +70,8 @@ public class FakeAuctionServer {
     }
 
     public void announceClosed() throws XMPPException {
-        currentChat.sendMessage(new Message());
+//        currentChat.sendMessage(new Message());
+        currentChat.sendMessage("SOLVersion: 1.1; Event:CLOSE;");
     }
 
     public void stop() {
