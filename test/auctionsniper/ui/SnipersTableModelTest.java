cd6aa3715c27f94c72c991cf30bbcb6b5edc4af1
성주현 <espel@onestorecorp.com>
2022-03-16 21:58:44 +0900

    15.3.1 잠깐의 휴식 p184 SniperState 를 SniperSnapShot으로 용어 변경하기.

--- test/auctionsniper/ui/SnipersTableModelTest.java
+++ test/auctionsniper/ui/SnipersTableModelTest.java
@@ -1,6 +1,6 @@
 package auctionsniper.ui;
 
-import auctionsniper.SniperState;
+import auctionsniper.SniperSnapshot;
 import org.hamcrest.Matcher;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
@@ -39,13 +39,13 @@ public class SnipersTableModelTest {
             one(listener).tableChanged(with(aRowChangedEvent()));
         }});
 
-        model.sniperStatusChanged(new SniperState("item id", 555, 666),
+        model.sniperStatusChanged(new SniperSnapshot("item id", 555, 666),
                 MainWindow.STATUS_BIDDING);
 
         assertColumnEquals(Column.ITEM_IDENTIFIER, "item id");
         assertColumnEquals(Column.LAST_PRICE, 555);
         assertColumnEquals(Column.LAST_BID, 666);
-        assertColumnEquals(Column.SNIPER_STATUS, MainWindow.STATUS_BIDDING);
+        assertColumnEquals(Column.SNIPER_STATE, MainWindow.STATUS_BIDDING);
     }
 
     private void assertColumnEquals(Column column, Object expected) {
